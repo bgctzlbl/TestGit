403641b535984e36bb23ad6e4920d606ac6b83c0
Zhan <2275058399@qq.com>
2025-08-25 18:31:52 +0800

    newtest01

---
+++ src/main/java/com/example/testgit/add.java
@@ -0,0 +1,11 @@
+package com.example.testgit;
+
+public class add {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hhh");
+        System.out.println("world");
+        System.out.println("hhh");
+        System.out.println("hhh");
+    }
+}
---
+++ src/main/java/com/example/testgit/commit.java
@@ -0,0 +1,7 @@
+package com.example.testgit;
+
+public class commit {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
