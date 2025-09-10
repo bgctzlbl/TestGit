198809bf4fa5022a9d442ff48c4601d7b8ec1ec4
Zhan <2275058399@qq.com>
2025-09-10 22:53:56 +0800

    testcherrypick01

---
+++ src/main/java/com/example/testgit/demos/web/Test.java
@@ -0,0 +1,8 @@
+package com.example.testgit.demos.web;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hhhh");
+        System.out.println(hhh);
+    }
+}
