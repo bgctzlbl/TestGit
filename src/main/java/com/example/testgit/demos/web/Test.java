fdd90ba016fd6a7666211830d06dd1a9ce9a5c3c
Zhan <2275058399@qq.com>
2025-09-10 23:11:36 +0800

    modification

--- src/main/java/com/example/testgit/demos/web/Test.java
+++ src/main/java/com/example/testgit/demos/web/Test.java
@@ -3,6 +3,6 @@ package com.example.testgit.demos.web;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hhhh");
-        System.out.println(hhh);
+        System.out.println("hhh");
     }
 }
